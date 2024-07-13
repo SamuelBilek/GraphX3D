1ce2821606d59e59ca14465700cee280c3c06309
Samuel Bilek <sam@polygoniq.com>
2024-07-13 21:26:56 +0200

    Initial window

---
+++ GraphicsApp/GraphicsApp/Game.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace GraphicsApp
+{
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string title)
+            : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title })
+        {
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+    }
+}
